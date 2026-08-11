cc750440ee413b5aa9f14168137ee6104fce6dc3
weirdbeardgame <obarrtimothy@gmail.com>
2026-08-10 20:16:13 -0500

    Added missing definitions Moved some typedefs in common to typedefs.h and included typedefs in common.h

--- include/typedefs.h
+++ include/typedefs.h
@@ -19,6 +19,41 @@ typedef int sceVu0IVECTOR[4] __attribute__((aligned (16)));
 typedef float sceVu0FVECTOR[4] __attribute__((aligned (16)));
 typedef float sceVu0FMATRIX[4][4] __attribute__((aligned (16)));
 
+
+
+typedef char s8;
+typedef short s16;
+typedef int s32;
+typedef long s64;
+
+typedef volatile s8 vs8;
+typedef volatile s16 vs16;
+typedef volatile s32 vs32;
+typedef volatile s64 vs64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long u64;
+
+typedef volatile u8 vu8;
+typedef volatile u16 vu16;
+typedef volatile u32 vu32;
+typedef volatile u64 vu64;
+
+typedef float f32;
+
+typedef union {
+    vs8 s8;
+    vs16 s16;
+    vs32 s32;
+    vs64 s64;
+    vu8 u8;
+    vu16 u16;
+    vu32 u32;
+    vu64 u64;
+} _VX;
+
 typedef union {
 	int ui32;
 	float fl32;
--- src/cs4/Seimei/src/File.h
+++ src/cs4/Seimei/src/File.h
@@ -1,5 +1,6 @@
 #ifndef FILE_H
 #define FILE_H
+#include "common.h"
 
 extern unsigned char ScePath[128]; // size: 0x80, address: 0x3254B0
 extern unsigned char RootPath[128]; // size: 0x80, address: 0x325530
@@ -8,5 +9,6 @@ void InitFileRead();
 void InitFileSystem();
 void ReadRootPath();
 unsigned char * GetRootPath();
+void AfterFileReadFunc(u8 *lpAddr, s32 index, s32 fileType, s32 size);
 
 #endif // FILE_H
